bb0451e1184c6b67a73b2d82a1265506425faeb3
Wikan-dev <oneshootindo@gmail.com>
2026-05-14 19:34:19 +0800

    refractor: rearange the SimpleIconProps to make it more reusable for another component

--- src/components/ui/icons/SimpleIcon.tsx
+++ src/components/ui/icons/SimpleIcon.tsx
@@ -1,8 +1,8 @@
 import type { SimpleIconProps } from "../../../type/type"
 
-export default function SimpleIcon({ icon, iconWidth }: SimpleIconProps) {
+export default function SimpleIcon({ icon, iconWidth, border }: SimpleIconProps) {
     return (
-        <div className="p-3.5 border-2 border-border-default rounded-2xl w-fit">
+        <div className={`p-3.5 ${border ? "border-2 border-border-default" : ""} rounded-2xl w-fit`}>
             <img src={icon} alt="icon" style={{ width: iconWidth }} />
         </div>
     )
